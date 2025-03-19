1afbdca2f605cdcc316707ab914fec43d953613c
Jshap44 <jash3373@colorado.edu>
2025-03-19 17:25:09 -0600

    +

---
+++ wa/wa10/main.js
@@ -0,0 +1,46 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+const insertX = ["Willy the Goblin Big Daddy Father Christmas"];
+const insertY = ["the soup kitchen Disneyland the White House"];
+const insertZ = ["spontaneously combusted melted into a puddle on the sidewalk turned into a slug and crawled away"];
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+randomize.addEventListener('click', result);
+
+function result() {
+  if(customName.value !== '') {
+    const name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+  
+  let newStory = storyText;
+
+  let xword = ":insertx:";
+  let y
+  let xItem = randomValueFromArray();
+  let yItem = randomValueFromArray();
+  let zItem = randomValueFromArray();
+
+  if(newStory.includes(xword)){
+    xword = xItem;
+  }
+  if(newStory.includes(y))
+
+
+}
