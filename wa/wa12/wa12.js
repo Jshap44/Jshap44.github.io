bfb8df9065417821afd07b696d493c2dec54ff36
Jshap44 <jash3373@colorado.edu>
2025-04-10 15:00:20 -0600

    +

--- wa/wa12/wa12.js
+++ wa/wa12/wa12.js
@@ -6,25 +6,22 @@ let Punchline = '';
       const json = await response.json();
       if(!response.ok) {
         throw Error(response.statusText);
-      }
+    }
 
       document.querySelector('#setup').textContent = json.setup; 
       Punchline = json.punchline; 
-
       document.querySelector('#punchline').textContent = '';
       document.querySelector('#punchline').style.display = 'none';
       document.querySelector('#answerBtn').disabled = false;
-
     } catch(err) {
         console.log(err);
         alert("Fail");
     }
-      
 }
 
-    function showAnswer() {
-      document.querySelector('#punchline').textContent = Punchline; 
-      document.querySelector('#punchline').style.display = 'block'; 
-      document.querySelector('#answerBtn').disabled = true; 
+function showAnswer() {
+    document.querySelector('#punchline').textContent = Punchline; 
+    document.querySelector('#punchline').style.display = 'block'; 
+    document.querySelector('#answerBtn').disabled = true; 
 
-    }
+}
